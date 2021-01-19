ea7e930d2acc96714b1aa75988ce87d858703e8f
yohan oh <yohan.oh332@gmail.com>
2021-01-19 15:15:22 -0500

    Added Board and Piece Class

---
+++ Chess_part1/Board.rb
@@ -0,0 +1,22 @@
+
+
+class Board 
+
+    def initialize
+        @grid = Array.new(8) {Array.new(8)}
+        fill_board
+    end
+
+    def fill_board 
+        @grid[0].each_with_index { |e, i| @grid[0][i] = Piece.new}
+        @grid[1].each_with_index { |e, i| @grid[1][i] = Piece.new}
+        @grid[6].each_with_index { |e, i| @grid[6][i] = Piece.new}
+        @grid[7].each_with_index { |e, i| @grid[7][i] = Piece.new}
+    end
+
+    def move_piece(start_pos, end_pos)
+        if start_pos[0] < 8 && start_pos[0] >= [0] && start_pos[1] < 8  && start_pos[1] >= 0 
+
+    end
+
+end
