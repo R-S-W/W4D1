cf8921fd7e090e31e63ca852f25e883a908d35b5
yohan oh <yohan.oh332@gmail.com>
2021-01-19 14:13:54 -0500

    partner switch

--- class_inheritance/test.rb
+++ class_inheritance/test.rb
@@ -1,3 +1,40 @@
-class Inheritance
+class Employee
+
+    
+
+    def initialize(name, title, salary, boss)
+        @name = name
+        @title = title
+        @salary = salary
+        @boss = boss 
+    end
+
+    def bonus(multiplier)
+        bonus = @salary * multiplier
+    end
 
 end
+
+class Manager < Employee
+    attr_reader :employees
+
+    def initialize(name, title, salary, boss, employee)
+        super
+        @employees = employee
+    
+    end
+
+    def bonus(multiplier)
+        bonus = 0
+
+        @employees.each do |employee|
+            bonus += employee.bonus(multiplier)
+        end
+
+        bonus 
+    end
+
+end
+
+ned = Manager.new("Ned", "Founder", 1000000, nil)
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
