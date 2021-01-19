973a5182594e166b519cad913e7c719cbde6e6f2
Ray <raysidarta@gmail.com>
2021-01-19 14:41:44 -0500

    finish manager class

--- class_inheritance/test.rb
+++ class_inheritance/test.rb
@@ -1,5 +1,5 @@
 class Employee
-
+    attr_reader :salary
     
 
     def initialize(name, title, salary, boss)
@@ -18,23 +18,54 @@ end
 class Manager < Employee
     attr_reader :employees
 
-    def initialize(name, title, salary, boss, employee)
-        super
-        @employees = employee
+    def initialize(name, title, salary, boss, employees)
+        
+        @employees = employees
+        super(name,title,salary,boss)
     
     end
 
     def bonus(multiplier)
-        bonus = 0
-
-        @employees.each do |employee|
-            bonus += employee.bonus(multiplier)
-        end
-
-        bonus 
+        self.add_salaries*multiplier
     end
+
+    def add_employee(employee)
+        @employees << employee
+    end
+
+    def add_salaries
+        salaries = 0
+        self.employees.each do |employee|
+            if employee.is_a?(Array)
+                salaries += employee.salary + employee.add_salaries
+            else
+                salaries += employee.salary
+            end  
+        end
+        salaries
+    end
+
+
 
 end
 
-ned = Manager.new("Ned", "Founder", 1000000, nil)
-darren = Manager.new("Darren", "TA Manager", 78000, "Ned")
+
+
+shawna = Employee.new("Shawna", "TA", 12000, 'Darren')
+david = Employee.new("David", "TA", 10000, 'Darren')
+darren = Manager.new("Darren", "TA Manager", 78000, "Ned", [shawna, david])
+ned = Manager.new("Ned", "Founder", 1000000, nil, [darren])
+
+
+
+# p david
+# p " "
+# p ned
+# p " "
+# p ned.employees
+
+p ' '
+p shawna.bonus(0.05)
+p ned.bonus(5)
+p darren.bonus(5)
+p david.bonus(3)
